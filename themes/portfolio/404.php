58ec839233188e222b2f9031a36800ca76e80fd1
frmysantana <frmysantana@yahoo.com>
2018-08-27 13:43:41 -0400

    Added a 404 page and began responsive styling

---
+++ themes/portfolio/404.php
@@ -0,0 +1,7 @@
+<?php get_header(); ?>
+  <div class="not-found">
+    <h1 class="not-found__title">That page does not exist.</h1>
+    <a href="<?php echo site_url(); ?>" class="not-found__link">Return to the main site.</a>
+  </div>
+<?php get_footer(); ?>
+
